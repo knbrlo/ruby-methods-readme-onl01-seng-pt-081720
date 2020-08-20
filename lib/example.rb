1fe8c1746991ee2b5c2936455b611db0643a26ed
Ken Barlow <kenbrlow@gmail.com>
2020-08-20 03:02:11 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -38,18 +38,17 @@ def say_hello_world_ten_times
 end
 
 say_hello_world_ten_times
- 
+
 # ... The rest of the program
- 
+
 say_hello_world_ten_times
 
 def greeting
   puts "Hello World"
 end
- 
+
 greeting # Executing the method by name
 #=> "Hello World"
  
 greeting # Executing the method again
 #=> "Hello World"
-
